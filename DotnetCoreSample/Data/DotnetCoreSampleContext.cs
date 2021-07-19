f071168f5c29fc6a85c7e0d073c448dbea7670e9
itayyab0o1 <tayyab.ismail22@gmail.com>
2021-07-19 21:53:25 +0500

    Add project files.

---
+++ DotnetCoreSample/Data/DotnetCoreSampleContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DotnetCoreSample;
+
+namespace DotnetCoreSample.Data
+{
+    public class DotnetCoreSampleContext : DbContext
+    {
+        public DotnetCoreSampleContext (DbContextOptions<DotnetCoreSampleContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<DotnetCoreSample.User> User { get; set; }
+    }
+}
---
+++ DotnetCoreSample/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DotnetCoreSample
+{
+    public class User
+    {
+        public long id { get; set; }
+
+        public string email { get; set; }
+
+        public string name { get; set; }
+    }
+}
